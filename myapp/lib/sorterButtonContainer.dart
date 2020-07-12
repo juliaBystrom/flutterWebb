4918d5673a26be53c309ba830552333ecc85c435
Julia Byström <juliabys@kth.se>
2020-07-12 18:10:32 +0200

    Implemented 3 microsecond delay forvisualislising an block being put in the sorted subarray (implementation sort)

--- myapp/lib/sorterButtonContainer.dart
+++ myapp/lib/sorterButtonContainer.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 class SorterButtonContainer extends StatelessWidget {
   Function insertionSort;
   Function insertionSortOneStep;
   SorterButtonContainer(this.insertionSort, this.insertionSortOneStep);
+
+   
 
   @override
   Widget build(BuildContext context) {
@@ -25,10 +28,11 @@ class SorterButtonContainer extends StatelessWidget {
           ),
         ),
         RaisedButton(
-          onPressed: () {
+          onPressed: () async {
             int flag = 0;
             int i = 1;
             while (flag == 0 && flag != 1) {
+              await Future.delayed(Duration(microseconds: 3));
               flag = insertionSortOneStep(i);
               print("Step " + i.toString() + " is now sorted");
               i++;
